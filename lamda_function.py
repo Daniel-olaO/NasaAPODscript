a5c4573ab3f377878b857911df7ae1af7159a323
Daniel-olaO <danieladedeji135@gmail.com>
2022-11-15 12:23:16 -0500

    Commit #2

--- lamda_function.py
+++ lamda_function.py
@@ -1,15 +1,11 @@
 import requests
 
-def runNasaApi():
-    url = "http://localhost:8000/api/send-apod"
+def lambda_handler(event, context):
     try:
-        r = requests.get(url)
-        return r.json()
+        res = requests.get("http://localhost:8000/api/send-apod")
+        return res.json()
     except Exception as e:
         return None
-
-def lambda_handler(event, context):
-    return runNasaApi()
 
 if __name__ == "__main__":
     lambda_handler(None, None)
